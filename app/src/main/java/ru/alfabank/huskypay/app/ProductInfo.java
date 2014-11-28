2a328df75fdba2b76bd4038ccc54c78f4bb07d5f
andrey <andrewmove@gmail.com>
2014-11-28 15:54:52 +0300

    добавил класс ProductInfo

--- app/src/main/java/ru/alfabank/huskypay/app/ProductInfo.java
+++ app/src/main/java/ru/alfabank/huskypay/app/ProductInfo.java
@@ -5,11 +5,13 @@ package ru.alfabank.huskypay.app;
  * @since 28.11.2014
  */
 public class ProductInfo {
+    private final long id;
     private final String name;
     private final String details;
     private final int cost;
 
-    public ProductInfo(String name, String details, int cost) {
+    public ProductInfo(long id, String name, String details, int cost) {
+        this.id = id;
         this.name = name;
         this.details = details;
         this.cost = cost;
