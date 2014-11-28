da79b259de2d0b8721d82b85fb610308f87a47a0
andrey <andrewmove@gmail.com>
2014-11-28 15:46:38 +0300

    сканирование бар кода с помощью zxing

--- app/src/main/java/ru/alfabank/huskypay/app/MainActivity.java
+++ app/src/main/java/ru/alfabank/huskypay/app/MainActivity.java
@@ -1,39 +1,38 @@
 package ru.alfabank.huskypay.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import com.google.zxing.integration.android.IntentIntegrator;
+
+import static com.google.zxing.client.android.Intents.Scan;
 
 
 public class MainActivity extends Activity {
+
+    private static final int SCAN_INTENT_REQUEST_CODE = 0;
+    private IntentIntegrator zxingIntegrator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        zxingIntegrator = new IntentIntegrator(this);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void handleAddButtonClick(View view) {
+        Intent scanIntent = zxingIntegrator.createScanIntent();
+        scanIntent.putExtra(Scan.MODE, Scan.ONE_D_MODE);
+        startActivityForResult(scanIntent, SCAN_INTENT_REQUEST_CODE);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == SCAN_INTENT_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                String contents = intent.getStringExtra(Scan.RESULT);
+            }
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 }
