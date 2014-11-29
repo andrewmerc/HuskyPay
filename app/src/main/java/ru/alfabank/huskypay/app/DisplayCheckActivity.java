864889443d3d0a3caf8f682512eb26a2fefdad55
atomheart <atomheart@yandex.ru>
2014-11-29 08:24:14 +0300

    Добавлено окно просмотра чека.

--- app/src/main/java/ru/alfabank/huskypay/app/DisplayCheckActivity.java
+++ app/src/main/java/ru/alfabank/huskypay/app/DisplayCheckActivity.java
@@ -1,7 +1,16 @@
 package ru.alfabank.huskypay.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Map;
 
 
 public class DisplayCheckActivity extends Activity {
@@ -21,6 +30,39 @@ public class DisplayCheckActivity extends Activity {
         super.onResume();
 
         paymentInfo = (PaymentInfo) getIntent().getExtras().getSerializable(PAYMENT_INFO_EXTRAS);
+        String date = new SimpleDateFormat("dd.MM.yyyy").format(paymentInfo.getPaymentDate());
+        String time = new SimpleDateFormat("HH:mm").format(paymentInfo.getPaymentDate());
 
+        String list = "";
+        Map<String, BigDecimal> details = paymentInfo.getBoughtProducts();
+        for (String detailName : details.keySet()) {
+            BigDecimal detailValue = details.get(detailName);
+            list += String.format(
+                    "<b>%s</b> 1 шт. %s руб.<br>",
+                    detailName,
+                    detailValue.toString()
+            );
+        }
+
+        String htmlTemplate = String.format(
+                "<b>Дата:</b> %s г.<br>\n<b>Время:</b> %s<br>\n<b>Продавец:</b> %s<br>\n<b>Сумма чека:</b><br>\n<br>\n———————<br>\n%s——————<br>\n<b>ИТОГ:</b> %s руб.<br>\n<b>В т.ч. НДС:</b> %s руб.<br>",
+                date,
+                time,
+                paymentInfo.getPartnerName(),
+                list,
+                paymentInfo.getPaymentSum().toString(),
+                paymentInfo.getPaymentVat().toString()
+        );
+
+        TextView textOrderInfo = (TextView) findViewById(R.id.textOrderInfo);
+        textOrderInfo.setText(Html.fromHtml(htmlTemplate));
+
+        ImageView img = (ImageView) findViewById(R.id.imageOrderQRCode);
+        img.setImageBitmap(paymentInfo.getCheckQRCode());
+    }
+
+    public void handleReturnButton(View v) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
